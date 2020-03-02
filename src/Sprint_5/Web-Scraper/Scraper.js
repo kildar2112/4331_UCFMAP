aab0e42b6c0c214b885952d1fea8ceb7f132f45b
Nick Plympton <kildar2112@gmail.com>
2020-03-01 20:46:48 -0500

    Scraper code semicolon edits and {} formatting

--- src/Sprint_5/Web-Scraper/Scraper.js
+++ src/Sprint_5/Web-Scraper/Scraper.js
@@ -4,66 +4,74 @@
 // Cheerio and request are used here to access and read the parking website.
 // Run npm i cheerio and nmp i request to install them if you do not have them already.
 
-const request = require('request')
-const cheerio = require('cheerio')
-const fs = require('fs')
-const writeStream = fs.createWriteStream("ScrapedData.csv")
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream("ScrapedData.csv");
 
 // Headers for Output
-writeStream.write(`Garage,Spaces Available,Percent Full \n`)
+writeStream.write(`Garage,Spaces Available,Percent Full \n`);
 
 // A garage has a name, empty spots, capacity, and percent full
-class Garage {
-  constructor(name, emptySpots, capacity) {
+class Garage
+{
+  constructor(name, emptySpots, capacity)
+	{
     this.name = name;
-    this.emptySpots = emptySpots
-    this.capacity = capacity
-    this.percentFull = Math.round((capacity - emptySpots) / capacity * 100)
+    this.emptySpots = emptySpots;
+    this.capacity = capacity;
+    this.percentFull = Math.round((capacity - emptySpots) / capacity * 100);
 
     if (this.percentFull < 0)
-      this.percentFull = 0
+      this.percentFull = 0;
   }
 }
 
 // Use cheerio to load the html from the website.
-function fillGarageInfo(html) {
-  const $ = cheerio.load(html)
-  var garages = []
-  var name = ''
+function fillGarageInfo(html)
+{
+  const $ = cheerio.load(html);
+  var garages = [];
+  var name = '';
 
   // Loop through every class with a dxgv tag, each of which
   // contains a name or parking data for a specific garage.
-  $('.dxgv').each((index, element) => {
-    const garageInfo = $(element).text()
+  $('.dxgv').each((index, element) =>
+	{
+    const garageInfo = $(element).text();
 
     // garageInfo returns an array with the text inside each dxgv class. However,
     // every text in garageInfo where index % 3 == 2 is empty. For example: garage[2] is empty.
     if (index % 3 == 0)
-      name = garageInfo
-    else if (index % 3 == 1) {
-      var fullnessInfo = garageInfo.trim().replace("\n","").split("/")
-      var emptySpots = parseInt(fullnessInfo[0])
-      var capacity = parseInt(fullnessInfo[1])
+      name = garageInfo;
+    else if (index % 3 == 1)
+		{
+      var fullnessInfo = garageInfo.trim().replace("\n","").split("/");
+      var emptySpots = Number(fullnessInfo[0]);
+      var capacity = Number(fullnessInfo[1]);
 
-      garages.push(new Garage(name, emptySpots, capacity))
+      garages.push(new Garage(name, emptySpots, capacity));
     }
 
-  })
+  });
 
-  return garages
+  return garages;
 }
 
 // Opens the request and gets a list of garages from fillGarageInfo()
 // It then writes all the information into ScrapedData.csv
-function scrape() {
-  request('http://secure.parking.ucf.edu/GarageCount/iframe.aspx', (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      var garages = fillGarageInfo(html)
+function scrape()
+{
+  request('http://secure.parking.ucf.edu/GarageCount/iframe.aspx', (error, response, html) =>
+	{
+    if (!error && response.statusCode == 200)
+		{
+      var garages = fillGarageInfo(html);
 
       for (var i = 0; i < garages.length; i++)
-        writeStream.write(`${garages[i].name}, ${garages[i].emptySpots} / ${garages[i].capacity}, ${garages[i].percentFull}%\n`)
+        writeStream.write(`${garages[i].name}, ${garages[i].emptySpots} / ${garages[i].capacity}, ${garages[i].percentFull}%\n`);
     }
-  })
+  });
 }
 
-scrape()
+scrape();
