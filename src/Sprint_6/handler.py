52b7555b95cf0a9434c2220924ab518f1109244a
Pablo <pablotrivino999@gmail.com>
2020-03-08 21:55:17 -0400

    Added package, handler, and serverless

--- src/Sprint_6/handler.py
+++ src/Sprint_6/handler.py
@@ -2,7 +2,10 @@
 # to read the HTML of the parking website and get the information of each garage, so
 # you might have to install it to run this.
 
-from urllib.request import urlopen as uReq
+import boto3
+import json
+import datetime
+import requests
 from bs4 import BeautifulSoup as soup
 
 class Garage:
@@ -10,29 +13,28 @@ class Garage:
         self.name = name
         self.empty_spots = empty_spots
         self.max_capacity = max_capacity
-        self.percent_full = round(empty_spots / max_capacity * 100)
+        self.percent_full = 100 - round(empty_spots / max_capacity * 100)
+
+        if self.percent_full < 0:
+            self.percent_full = 0
 
     def __str__(self):
-        return self.name + "\t" + str(self.empty_spots) + "/" + str(self.max_capacity) + "\t" + str(100 - self.percent_full) + "%\n"
+        return self.name + "\t" + str(self.empty_spots) + "/" + str(self.max_capacity) + "\t" + str(self.percent_full) + "%\n"
 
 # Opens a request to the parking website and gets the HTML
 def getPageHTML():
     garageCount_url = 'http://secure.parking.ucf.edu/GarageCount/iframe.aspx';
-
-    uClient = uReq(garageCount_url)
-    page_html = uClient.read()
-    uClient.close()
-
+    page_html = requests.get(garageCount_url)
     return page_html
 
 # Returns an array in which every element contains the HTML for a garage. For example,
 # array[0] contains the HTML for Garage A
 def getHTMLForAllGarages(page_html):
-    page_soup = soup(page_html, "html.parser")
+    page_soup = soup(page_html.content, "html.parser")
     return page_soup.findAll("tr", {"class" : "dxgvDataRow_DevEx"})
 
 # Removes the newlines, spaces, and carriage returns from the garage_space text
-# Returns an array with two elements: the filled spots and the max capacity
+# Returns an array with two elements: the empty spots and the max capacity
 def formatGarageSpace(garage_space):
     garage_space = garage_space.replace("\r", "")
     garage_space = garage_space.replace("\n", "")
@@ -41,7 +43,7 @@ def formatGarageSpace(garage_space):
     return garage_space.split("/")
 
 # Creates a list of garages, which each contain their name and fullness
-def generateGarageList(garage_list):
+def generateGarages(garage_list):
     garages = []
     for garage in garage_list:
         garage_information = garage.findAll("td", {"class" : "dxgv"})
@@ -55,13 +57,27 @@ def generateGarageList(garage_list):
     return garages
 
 # Method that calls all the relevant methods to scrape data
-def scrape():
+def scrapeGarages():
     page_html = getPageHTML()
     garage_list = getHTMLForAllGarages(page_html)
-    garages = generateGarageList(garage_list)
-    return garages
+    garages = generateGarages(garage_list)
+    return {
+        'date' : str(datetime.datetime.now()),
+        'Garage A' : str(garages[0].percent_full),
+        'Garage B' : str(garages[1].percent_full),
+        'Garage C' : str(garages[2].percent_full),
+        'Garage D' : str(garages[3].percent_full),
+        'Garage H' : str(garages[4].percent_full),
+        'Garage I' : str(garages[5].percent_full),
+        'Garage Libra' : str(garages[6].percent_full),
+    }
 
-garages = scrape()
+def saveFile(bucket, filename, data):
+    s3 = boto3.resource('s3')
+    object = s3.Object(bucket, filename)
+    object.put(Body = json.dumps(data))
 
-for garage in garages:
-    print(garage)
+def scrape(event, context):
+    data = scrapeGarages()
+    filename = f"garageInfo-{data['date']}"
+    saveFile('garage_info', filename, data)
